f7193466a20fe1084e4868c91dc74447360b42a8
Erich Polixa <epolixa@gmail.com>
2015-12-02 11:43:40 -0500

    reorganized some php pages

---
+++ php/head.php
@@ -0,0 +1,18 @@
+<!--  head.php
+      link scripts and styles here
+      Erich
+-->
+
+<head>
+  <title>CS 546 Airport Aggregator Project</title>
+
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="../css/base.css">
+
+  <!-- JS -->
+  <script type="text/javascript" src="../js/jquery-1.11.3.min.js"></script>
+  <script type="text/javascript" src="../js/effects.js"></script>
+  <script type="text/javascript" src="../js/sha512.js"></script>
+  <script type="text/javascript" src="../js/forms.js"></script>
+  <script type="text/javascript" src="../js/checker.js"></script>
+</head>
--- php/header.php
+++ php/header.php
@@ -1,56 +1,25 @@
-<?php
-require_once('../includes/Common.php');
-  $airportID = filter_input(INPUT_GET,'airport',FILTER_SANITIZE_NUMBER_INT);
-  $airport = new Airport($airportID);
- ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>CS 546 Airport Aggregator Project</title>
+<!--  header.php
+      header structure
+      Erich
+-->
 
-    <script type="text/javascript" src="../js/jquery-1.11.3.min.js"></script>
-    <script type="text/javascript" src="../js/global.js"></script>
-    <script type="text/javascript" src="../js/sha512.js"></script> 
-    <script type="text/javascript" src="../js/forms.js"></script> 
-    <script type="text/javascript" src="../js/checker.js"></script>
-    <link rel="stylesheet" type="text/css" href="../css/base.css">
-  </head>
+<div class="header-wrap">
+  <?php require_once('../includes/Common.php') ?>
 
-  <body>
-
-    <div id="airport" class="container">
-      <div class="header-wrap">
-        <div class="header">
-          <nav class="nav-main">
-            <ul>
-              <li><a href="../home.html">Home</a></li>
-              <li><a href="../about.html">About</a></li>
-              <li class="airports">
-                  <?php Common::airport_nav_dropdown(); ?>
-              </li>
-              <li>
-                <a href="./register.php">Register</a>
-                |<a href="./login.php">Login</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-
-        <div class="content-wrap">
-          <div class="content-left">
-            <nav class="nav-secondary">
-              <ul>
-                <li><h3><?php echo $airport->name(); ?> International Airport</h3></li>
-                <li><a href="airport.php?airport=<?php echo $airport->id(); ?>">Home</a></li>
-                <li><a href="history.php?airport=<?php echo $airport->id(); ?>">History</a></li>
-                <li><a href="flights.php?airport=<?php echo $airport->id(); ?>">Flights</a></li>
-                <li><a href="shopping.php?airport=<?php echo $airport->id(); ?>">Shopping & Food</a></li>
-                <li><a href="rentals.php?airport=<?php echo $airport->id(); ?>">Car Rentals</a></li>
-                <li><a href="parking.php?airport=<?php echo $airport->id(); ?>">Parking</a></li>
-                <li><a href="airlines.php?airport=<?php echo $airport->id(); ?>">Airlines</a></li>
-                <li><a href="reviews.php?airport=<?php echo $airport->id(); ?>">Reviews</a></li>
-                <li><a href="faq.php?airport=<?php echo $airport->id(); ?>">FAQ</a></li>
-                <li><a href="contact.php?airport=<?php echo $airport->id(); ?>">Contact</a></li>
-              </ul>
-            </nav>
-          </div>
+  <div class="header">
+    <nav class="nav-main">
+      <ul>
+        <li><a href="../home.html">Home</a></li>
+        <li><a href="../about.html">About</a></li>
+        <li class="airports">
+            <?php Common::airport_nav_dropdown(); ?>
+        </li>
+        <li>
+          <a href="./register.php">Register</a>
+          |
+          <a href="./login.php">Login</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>
